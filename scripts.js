3cf27f1dbafd9ca9ae224e99141d75594fdb7387
Ryan Menning <rmenning43@gmail.com>
2021-12-10 03:11:44 -0500

    sleepy

--- scripts.js
+++ scripts.js
@@ -4,6 +4,30 @@ function toggleMenu() {
 let x = document.getElementById('hamburgerBtn');
 x.onclick = toggleMenu; 
 
+
+function spinWheel() {
+    // document.getElementById("box2").style.transform = "rotate(360deg)" ;
+    
+    let min = 1;
+    let max = 12;
+    let count = 0;
+    let myArr = [1,2,3,4,5,6,7,8,9,10,11,12]
+    let meal = document.getElementById("hello").textContent;
+    for (i=6;i!=0;i--) {
+        let randNum = Math.floor(Math.random() * (max - min + 1) + min);
+        
+        // let meal = document.getElementById(randNum.toString()).textContent;
+        // document.getElementById("day-" + count.toString() + "-meal").innerHTML = meal
+        console.log(randNum);
+        console.log(i);
+        console.log(meal);
+
+    }
+    
+}
+
+
+
 function populateDays() {
     const d = new Date();
     const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
@@ -13,14 +37,12 @@ function populateDays() {
     for (let i = curDay; i<7; i++) {
         document.getElementById("day-" + count.toString()).innerHTML = "Your " + weekday[i] + " Dinner";
         count++;
-        console.log(count);
     }
 
     if (count < 7) {
-        for (let j = 0; j!=curDay; j++) {
-            document.getElementById("day-" + count.toString()).innerHTML = "Your " + weekday[j] + " Dinner";
+        for (let i = 0; i!=curDay; i++) {
+            document.getElementById("day-" + count.toString()).innerHTML = "Your " + weekday[i] + " Dinner";
             count++;
-            console.log(count);
         }
     }
     
